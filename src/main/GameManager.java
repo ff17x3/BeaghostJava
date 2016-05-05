2c11428794a88e7d2aecb06c0c175e863be416e9
ff17x3 <ff17x3@gmail.com>
2016-05-05 17:55:17 +0200

    flo

--- src/main/GameManager.java
+++ src/main/GameManager.java
@@ -1,12 +1,49 @@
 package main;
 
+import util.ClockNano;
+import util.Tickable;
+
+import java.awt.*;
+import java.util.ArrayList;
+
 /**
- * Created by Florian on 05.05.2016.
+ * Created by Florian on 05.05.2016
  */
 public class GameManager {
+
+	// params:
+	public static final int FPS = 60;
+
 	private Main main;
+	private ClockNano drawClock, tickClock;
+	private ArrayList<Robot> robots = new ArrayList<>();
 
 	public GameManager(Main main) {
 		this.main = main;
+
+		//TODO spawn robots
+
+		drawClock = new ClockNano(FPS, (Tickable) millisDelta -> {
+			for (Robot r : robots) {
+				r.draw();
+			}
+
+		});
+		tickClock = new ClockNano(FPS, (Tickable) millisDelta -> {
+			for (Robot r : robots) {
+				r.tick();
+			}
+		});
+	}
+
+
+
+
+	public void startTicking() {
+
+	}
+
+	public void stopTicking() {
+
 	}
 }
---
+++ src/main/Player.java
@@ -0,0 +1,11 @@
+package main;
+
+/**
+ * Created by Florian on 05.05.2016.
+ */
+public class Player {
+	float x,y,dir;
+	public Player(){
+
+	}
+}
