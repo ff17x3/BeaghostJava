b207c28d2f63753dba2ca404b6916d9c057ab828
Ma <machrivi99@gmail.com>
2016-05-05 17:55:12 +0200

    m rebase

--- src/main/Robot.java
+++ src/main/Robot.java
@@ -1,20 +1,82 @@
 package main;
 
+import java.awt.*;
+
 /**
- * Created by Florian on 05.05.2016.
+ * Created by Ma on 05.05.2016
  */
-public class Robot extends Entity {
-	public static final float RADIUS = 5;
+public class Robot {
+    // ZEICHNEN +++++++++++++++++++++++++++++++++++++++++++++++
 
-	private Robot(float x, float y, float dir) {
-		super(x, y, dir);
-	}
+    // statische Winkel für Boxen-----------------------------------------------
+    private static final float[] angles; // Winkel für Ecken von Boxen
+
+    static {
+        // angles init
+        angles = new float[8];
+        angles[0] = pi / 4;
+        angles[1] = a;
+        angles[2] = pi - a;
+        angles[3] = 0.75f * pi;
+        angles[4] = 1.25f * pi;
+        angles[5] = pi + a;
+        angles[6] = 2 * pi - a;
+        angles[7] = 1.75f * pi;
+    }
+
+    // -----------------------------------------------------------------------
+    private final float distA = (float) (Math.sqrt(2) * radius), distB = (float) (Math.sqrt(4.0625) * radius); // TODO anpassen, wenn sich radius ändert
+
+    private Polygon poly = new Polygon(); //
+    // Entfernungen zu den Ecken der Boxen zum Zeichnen
+    private float[] angleSins = new float[8], angleCosins = new float[8];
+
+    private boolean initOK = false;
+    public SimpleRobot(float x, float y, float dir, GameManager gm, int bodyColor) {
+        // colors
+        bodyPaint.setColor(bodyColor); // body: 0xff990000 box: 0xff000000
+    }
+
+    @Override
+    public void draw(Canvas c) {
+        // Blöcke links und rechts
+        // rechter Block
+        drawPath.reset();
+        drawPath.moveTo(angleCosins[0] * distA + x, angleSins[0] * distA + y);
+        drawPath.lineTo(angleCosins[1] * distB + x, angleSins[1] * distB + y);
+        drawPath.lineTo(angleCosins[2] * distB + x, angleSins[2] * distB + y);
+        drawPath.lineTo(angleCosins[3] * distA + x, angleSins[3] * distA + y);
+        drawPath.close();
+        c.drawPath(drawPath, boxPaint);
+        // linker Block
+        drawPath.reset();
+        drawPath.moveTo(angleCosins[4] * distA + x, angleSins[4] * distA + y);
+        drawPath.lineTo(angleCosins[5] * distB + x, angleSins[5] * distB + y);
+        drawPath.lineTo(angleCosins[6] * distB + x, angleSins[6] * distB + y);
+        drawPath.lineTo(angleCosins[7] * distA + x, angleSins[7] * distA + y);
+        drawPath.close();
+        c.drawPath(drawPath, boxPaint);
+        // sichtfeld
+        drawViewField(fov / 2, fov, c);
+        // Körper
+        c.drawCircle(x, y, radius, bodyPaint);
+    }
 
 
-	public static Robot spawnRandom(float mapWidth, float mapHeight) {
-		float x = (float) (Math.random() * (mapWidth - 2 * RADIUS) + RADIUS);
-		float y = (float) (Math.random() * (mapHeight - 2 * RADIUS) + RADIUS);
-		float dir = (float) (Math.random() * (2 * Math.PI));
-		return new Robot(x, y, dir);
-	}
+
+    @Override
+    protected void changeDir(float change) {
+        super.changeDir(change);
+        // Werte für Boxen an den Seiten an Winkel anpassen
+        for (int i = 0; i < angles.length; i++) {
+            float actangle = getDir() + angles[i];
+            actangle %= 2 * pi;
+            if(!initOK){
+                initOK = true;
+                angleSins = new float[8];
+                angleCosins = new float[8];}
+            angleSins[i] = (float) Math.sin(actangle);
+            angleCosins[i] = (float) Math.cos(actangle);
+        }
+    }
 }
