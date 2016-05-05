dc656ea3caecd3461bf5a670a0c15bf0f2321bbe
ff17x3 <ff17x3@gmail.com>
2016-05-05 17:04:10 +0200

    flo

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,36 @@
 package main;
 
+import util.DimensionF;
+import util.DrawFrame;
+import util.DrawInferface;
+import util.FrameInitInterface;
+
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * Created by Florian on 05.05.2016. 16:48
  */
-public class Main {
-    public static void main(String[] args) { // max
+public class Main implements DrawInferface, FrameInitInterface {
+	public static void main(String[] args) {
+		new Main();
+	}
 
-    }
+	private DrawFrame frame;
+	private Dimension frameSize = new Dimension(500, 500);
+	private DimensionF mapSize = new DimensionF(500, 500);
+
+	public Main() {
+		frame = new DrawFrame(frameSize, this, this, mapSize);
+	}
+
+	@Override
+	public void draw(Graphics g, float scale) {
+
+	}
+
+	@Override
+	public void initFrame(JFrame f, DrawFrame.DrawPanel dp) {
+
+	}
 }
