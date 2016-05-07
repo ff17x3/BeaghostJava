a0f360f6bdeb4f18ec8db4fe58ff2c4d9de8ec84
ff17x3 <ff17x3@gmail.com>
2016-05-07 17:12:23 +0200

    flo

--- src/main/KIRobot1.java
+++ src/main/KIRobot1.java
@@ -25,7 +25,8 @@ public class KIRobot1 extends Robot implements Entity.Tickable {
 	//drawing
 	private float dash[] = {10f, 20f};
 	private float lineThickness = 2f;
-	private BasicStroke dashedStroke;
+	private BasicStroke dashedStroke = new BasicStroke(lineThickness, BasicStroke.CAP_BUTT, BasicStroke.JOIN_MITER, 10f, dash, 0.0f);
+
 
 	public KIRobot1(float x, float y, float dir, GameManager gm) {
 		super(x, y, dir, gm);
